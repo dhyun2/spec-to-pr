9759f0462da411730dcc03e66dbe62b1afc8e047
Donghyun Park <dhp94d@gmail.com>
2026-06-28 02:10:48 +0900

    feat(app): add source registry service

--- src/application/index.ts
+++ src/application/index.ts
@@ -2,3 +2,4 @@ export * from "./run-service.js";
 export * from "./stage-service.js";
 export * from "./policy-service.js";
 export * from "./profile-service.js";
+export * from "./source-registry-service.js";
