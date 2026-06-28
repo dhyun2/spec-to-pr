fd0c5ea827917a683b46922a6002479a5a1232da
Donghyun Park <dhp94d@gmail.com>
2026-06-28 17:41:26 +0900

    build(architecture): support bundled typescript compiler

--- tsup.config.ts
+++ tsup.config.ts
@@ -15,6 +15,6 @@ export default defineConfig({
   minify: false,
   noExternal: [/@modelcontextprotocol\/sdk/, /zod/, /typescript/],
   banner: {
-    js: "#!/usr/bin/env node",
+    js: '#!/usr/bin/env node\nimport { createRequire as __createRequire } from "node:module";\nimport { fileURLToPath as __fileURLToPath } from "node:url";\nimport { dirname as __dirnameOf } from "node:path";\nconst require = __createRequire(import.meta.url);\nconst __filename = __fileURLToPath(import.meta.url);\nconst __dirname = __dirnameOf(__filename);',
   },
 });
