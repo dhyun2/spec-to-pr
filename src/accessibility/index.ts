6a4e96780555bf4ccfd553562f4913097e4743b5
Donghyun Park <dhp94d@gmail.com>
2026-06-28 18:14:50 +0900

    feat(accessibility): add target discovery and axe result normalizer

---
+++ src/accessibility/index.ts
@@ -0,0 +1,7 @@
+export * from "./accessibility-gap-mapper.js";
+export * from "./accessibility-model.js";
+export * from "./accessibility-report-renderer.js";
+export * from "./accessibility-targets.js";
+export * from "./axe-result-normalizer.js";
+export * from "./focus-checks.js";
+export * from "./keyboard-smoke.js";
